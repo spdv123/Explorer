746550de367de55233ce1e284acf2a81737dfacc
spdv123 <tianshen477@live.com>
2018-12-17 18:46:51 +0800

    修复函数缺失

--- config.php
+++ config.php
@@ -1,15 +1,29 @@
 <?php
+	function file_rep_contents($filename, $arr) {
+			$content = file_get_contents($filename);
+
+			$rep_before = array();
+			$rep_after = array();
+			foreach ($arr as $key => $value) {
+				$rep_before[] = "{data.".$key."}";
+				$rep_after[] = $value;
+			}
+			$content = str_replace($rep_before, $rep_after, $content);
+			$content = preg_replace("/{data[._0-9a-zA-Z-]+}/", "", $content);
+
+			return $content;
+	}
 	error_reporting(0);
 	ob_start();
 	session_start();
-	$dblink=@mysql_connect("localhost","用户名","密码");
+	$dblink=@mysql_connect("localhost","USERNAME","PASSWORD");
 	if($dblink==null)
 	{
-		echo "数据库打开失败";
+		echo "open database error";
 		exit;
 	}
 	mysql_query("SET NAMES 'utf8'");
   mysql_query("set character_set_client=utf8");
   mysql_query("set character_set_results=utf8");
-	mysql_select_db("数据库名");
+	mysql_select_db("DATABASE_NAME");
 ?>
